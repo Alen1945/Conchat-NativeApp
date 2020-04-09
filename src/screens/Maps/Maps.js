2c4a7462c3e9a00169b80c883d4a960f9be47b92
Alen1945 <dwialendraipangselly23@gmail.com>
2020-04-09 10:54:44 +0700

    show user current location

--- src/screens/Maps/Maps.js
+++ src/screens/Maps/Maps.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import geolocation from '@react-native-community/geolocation';
+import {Toast} from 'native-base';
 export default function Maps(props) {
+  const [currentPosition, setCurrentPosition] = React.useState(false);
+  React.useEffect(() => {
+    geolocation.getCurrentPosition(
+      (position) => {
+        if (position) {
+          console.log(position.coords);
+          setCurrentPosition(position.coords);
+        }
+      },
+      (err) => {
+        if (err.code === 2) {
+          Toast.show({
+            text: 'Please active your Location',
+            buttonText: 'Ok',
+            duration: 15000,
+            position: 'bottom',
+          });
+        }
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 60000,
+        maximumAge: 1000,
+      },
+    );
+  }, []);
   return (
     <View>
       <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: currentPosition
+            ? currentPosition.latitude
+            : -6.8205799999999995,
+          longitude: currentPosition
+            ? currentPosition.longitude
+            : 106.81851166666665,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
         }}>
         <Marker
           coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: currentPosition
+              ? currentPosition.latitude
+              : -6.8205799999999995,
+            longitude: currentPosition
+              ? currentPosition.longitude
+              : 106.81851166666665,
           }}
         />
       </MapView>
