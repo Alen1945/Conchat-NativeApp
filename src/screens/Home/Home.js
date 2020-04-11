c2f6d18e3558692f5c7f3cc720fd2b4507b39682
Alen1945 <dwialendraipangselly23@gmail.com>
2020-04-11 18:58:18 +0700

    search user

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  TouchableHighlight,
-} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import {
   Container,
   Content,
@@ -15,12 +10,14 @@ import {
   Right,
   Text,
 } from 'native-base';
-import {Avatar} from 'react-native-elements';
-import {auth, db} from '../../config/firebase';
+import {Avatar, Overlay, Input} from 'react-native-elements';
+import {auth, db, firestore} from '../../config/firebase';
 import Icon from 'react-native-vector-icons/FontAwesome';
 export default function Home(props) {
   const [idListChat, setIdListChat] = React.useState([]);
   const [listChat, setListChat] = React.useState({});
+  const [isVisibleOverlay, setIsVisibleOverlay] = React.useState(false);
+  const [listContact, setListContact] = React.useState([]);
   const getIdListChat = async () => {
     try {
       const listIdRoom = await db
@@ -74,67 +71,149 @@ export default function Home(props) {
       console.log(err);
     }
   };
+  const handleSearchContact = async (name = '') => {
+    try {
+      let data = {};
+      if (name.length > 1) {
+        const searchResult = await firestore
+          .collection('Users')
+          .where('displayName', '==', name)
+          .limit(30)
+          .get();
+        if (searchResult.docs) {
+          searchResult.docs.forEach((user) => (data[user.id] = user.data()));
+          console.log(data);
+          setListContact(data);
+        }
+      } else if (name.length === 0) {
+        const searchResult = await firestore.collection('Users').get();
+        if (searchResult.docs) {
+          searchResult.docs.forEach((user) => (data[user.id] = user.data()));
+          setListContact(data);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   React.useEffect(() => {
     getIdListChat();
+    handleSearchContact();
   }, []);
 
   React.useEffect(() => {
     getListChat();
   }, [idListChat]);
   return (
-    <Container>
-      <Content>
-        <List>
-          {Object.keys(listChat).length > 0 &&
-            Object.keys(listChat).map((idRoom, i) => {
-              const room = listChat[idRoom];
-              return (
-                <ListItem
-                  key={idRoom}
-                  avatar
-                  button
-                  onPress={() =>
-                    props.navigation.navigate('RoomChat', {
-                      idRoom: idRoom,
-                      titleRoom: room.titleRoom,
-                      iconRoom: room.iconRoom,
-                    })
-                  }>
-                  <Left>
-                    <Avatar
-                      title="U"
-                      size={50}
-                      rounded
-                      source={room.iconRoom && {uri: room.iconRoom}}
-                    />
-                  </Left>
-                  <Body>
-                    <Text>{room.titleRoom}</Text>
-                    <Text note>
-                      {room.lastMessage && room.lastMessage.substring(0, 15)}
-                      {room.lastMessage &&
-                        room.lastMessage.length > 15 &&
-                        '...'}
-                    </Text>
-                  </Body>
-                  <Right>
-                    <Text note>
-                      {new Date(room.lastAddedMessage).getHours() +
-                        ':' +
-                        new Date(room.lastAddedMessage).getMinutes()}
-                    </Text>
-                  </Right>
-                </ListItem>
-              );
-            })}
-        </List>
-      </Content>
-      <View style={style.containerAddChat}>
-        <TouchableOpacity style={style.btnAddChat}>
-          <Icon name="plus" size={25} color="#fff" />
-        </TouchableOpacity>
-      </View>
-    </Container>
+    <>
+      <Container>
+        <Content>
+          <List>
+            {Object.keys(listChat).length > 0 &&
+              Object.keys(listChat).map((idRoom, i) => {
+                const room = listChat[idRoom];
+                return (
+                  <ListItem
+                    key={idRoom}
+                    avatar
+                    button
+                    onPress={() =>
+                      props.navigation.navigate('RoomChat', {
+                        idRoom: idRoom,
+                        titleRoom: room.titleRoom,
+                        iconRoom: room.iconRoom,
+                      })
+                    }>
+                    <Left>
+                      <Avatar
+                        title="U"
+                        size={50}
+                        rounded
+                        source={room.iconRoom && {uri: room.iconRoom}}
+                      />
+                    </Left>
+                    <Body>
+                      <Text>{room.titleRoom}</Text>
+                      <Text note>
+                        {room.lastMessage && room.lastMessage.substring(0, 15)}
+                        {room.lastMessage &&
+                          room.lastMessage.length > 15 &&
+                          '...'}
+                      </Text>
+                    </Body>
+                    <Right>
+                      <Text note>
+                        {new Date(room.lastAddedMessage).getHours() +
+                          ':' +
+                          new Date(room.lastAddedMessage).getMinutes()}
+                      </Text>
+                    </Right>
+                  </ListItem>
+                );
+              })}
+          </List>
+        </Content>
+        <View style={style.containerAddChat}>
+          <TouchableOpacity
+            style={style.btnAddChat}
+            onPress={() => setIsVisibleOverlay(true)}>
+            <Icon name="plus" size={25} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      </Container>
+      <Overlay
+        isVisible={isVisibleOverlay}
+        overlayBackgroundColor="#fff"
+        overlayStyle={{
+          height: '100%',
+          width: '100%',
+        }}
+        width="auto"
+        height="auto">
+        <View style={{flex: 1, position: 'relative'}}>
+          <TouchableOpacity
+            style={{position: 'absolute', top: 0, left: 10}}
+            onPress={() => setIsVisibleOverlay(false)}>
+            <Icon name="times" size={27} color="#666" />
+          </TouchableOpacity>
+          <Text style={style.headingSearch}>Select User</Text>
+          <Input
+            placeholder="name"
+            onChangeText={(text) => handleSearchContact(text)}
+            inputContainerStyle={{...style.input}}
+            inputStyle={style.inputText}
+          />
+          {Object.values(listContact).length > 0 && (
+            <Content
+              style={{
+                marginTop: 20,
+              }}>
+              <List style={{paddingRight: 30}}>
+                {Object.values(listContact).map((user, i) => (
+                  <ListItem key={i} avatar button style={{marginVertical: 6}}>
+                    <Left>
+                      <Avatar
+                        title="U"
+                        size={50}
+                        rounded
+                        source={{uri: user.photoURL}}
+                      />
+                    </Left>
+                    <Body>
+                      <Text>{user.displayName}</Text>
+                      <Text note>{user.phoneNumber}</Text>
+                    </Body>
+                    <Right>
+                      <Text note>{user.isOnline ? 'Online' : 'Offline'}</Text>
+                    </Right>
+                  </ListItem>
+                ))}
+              </List>
+            </Content>
+          )}
+        </View>
+      </Overlay>
+    </>
   );
 }
 
@@ -154,5 +233,27 @@ const style = StyleSheet.create({
     borderRadius: 40,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headingSearch: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#666',
+  },
+  input: {
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'transparent',
+    borderBottomWidth: 0,
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 20,
+    backgroundColor: '#eaeaea',
+  },
+  inputText: {
+    fontSize: 13,
+    color: '#525252',
+    textAlign: 'center',
   },
 });
